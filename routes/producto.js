421ba66f2018cfd80bf575bb3cb6d9c1b9d24f47
Carolina Pineda Grifaldo <gpinedacarolina@gmail.com>
2023-09-14 14:43:47 -0600

    Asociaciones de los modelos

---
+++ routes/producto.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/producto', );
+
+router.post('/producto', );
+
+router.put('/producto/:id', );
+
+router.delete('/producto/:id', );
+
+router.get('/producto/:id', );
+
+export default router;
+
